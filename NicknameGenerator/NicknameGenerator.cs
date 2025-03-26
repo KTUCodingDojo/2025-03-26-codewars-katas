60dfeba56fb79d9bb0d0b6a102d5c2c71a3c637a
Evaldas Guogis <evaldasguogis@yahoo.com>
2025-03-26 18:51:13 +0200

    Removed one if check

--- NicknameGenerator/NicknameGenerator.cs
+++ NicknameGenerator/NicknameGenerator.cs
@@ -9,18 +9,20 @@ namespace NicknameGenerator
     public class NicknameGenerator
     {
         private static readonly char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        private static readonly Dictionary<bool, Func<string, string>> dict = new Dictionary<bool, Func<string, string>>();
+
+        public NicknameGenerator() 
+        {
+            dict[true] = x => x.Substring(0, 4);
+            dict[false] = x => x.Substring(0, 3);
+        }
 
         public string GenerateNickname(string nickname)
         {
             if (nickname.Length < 4) return "Error: Name too short";
 
             var is3rdLetterVowel = vowels.Contains(nickname.ElementAt(2));
-            if (is3rdLetterVowel)
-            {
-                return nickname.Substring(0, 4);
-            }
-
-            return nickname.Substring(0, 3);
+            return dict[is3rdLetterVowel](nickname);
         }
     }
 }
