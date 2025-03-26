e36b92e96fbcbf9b7421535e70808c894de67935
Evaldas Guogis <evaldasguogis@yahoo.com>
2025-03-26 19:18:22 +0200

    Converted dictionary to string array

--- NoIfsNoButs/Comparator.cs
+++ NoIfsNoButs/Comparator.cs
@@ -5,19 +5,12 @@ namespace NoIfsNoButs
 {
     public class Comparator
     {
-        static Dictionary<int, string> values = new Dictionary<int, string>
-        {
-            {0, "smaller than" },
-            {1,  "equal to"},
-            {2, "greater than" }
-        };
-        // values to array
-
+        private static string[] answers = { "smaller than", "equal to", "greater than" };
 
         public static string Compare(int a, int b)
         {
             int compareResult = a.CompareTo(b) + 1;
-            string result = $"{a} is {values[compareResult]} {b}";
+            string result = $"{a} is {answers[compareResult]} {b}";
             return result;
         }
     }
